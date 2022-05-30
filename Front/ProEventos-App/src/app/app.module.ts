754e90bf9a8efe89e9841a88ca4eb8ed7719735b
Oswaldo da Silva Schermach <oswaldo.schermach@weipa.com.br>
2022-05-30 08:31:24 -0300

    Add Http Request

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; //modulo http para fazer o request da api
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+      EventosComponent,
+      PalestrantesComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss']
+})
+export class EventosComponent implements OnInit {
+
+  public eventos: any;
+
+
+  constructor(private http: HttpClient) { }
+
+  public getEventos(): void {
+    this.eventos = [
+      {
+        Tema: "Angular",
+        Local: "Curitiba"
+      },
+      {
+        Tema: "C#",
+        Local: "Sao Paulo"
+      }
+    ]
+  }
+
+
+  ngOnInit() { //inicializa ao abrir a aplicação
+      this.getEventos();
+  }
+
+}
